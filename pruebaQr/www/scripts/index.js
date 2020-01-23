6024ac8357d25812855859649fd28ff0e3a0f98c
kikeSGV <kike@sgv.es>
2020-01-23 16:10:35 +0100

    cambio de plugin

--- pruebaQr/www/scripts/index.js
+++ pruebaQr/www/scripts/index.js
@@ -25,21 +25,26 @@
     $('#scanQR').click(function () {
         cordova.plugins.barcodeScanner.scan(
             function (result) {
-                if (!result.cancelled) {
-                    // En este caso solo queremos que procese código QR
-                    if (result.format == "QR_CODE") {
-                        var value = result.text;
-                        // Contenido del código QR
-                        alert(value);
-                    } else {
-                        alert("Ops, se escaneo un código pero al parecer no es QR");
-                    }
-                } else {
-                    alert("El usuario se ha saltado el escaneo.");
-                }
+                alert("We got a barcode\n" +
+                    "Result: " + result.text + "\n" +
+                    "Format: " + result.format + "\n" +
+                    "Cancelled: " + result.cancelled);
             },
             function (error) {
-                alert("Ha ocurrido un error: " + error);
+                alert("Scanning failed: " + error);
+            },
+            {
+                preferFrontCamera: true, // iOS and Android
+                showFlipCameraButton: true, // iOS and Android
+                showTorchButton: true, // iOS and Android
+                torchOn: true, // Android, launch with the torch switched on (if available)
+                saveHistory: true, // Android, save scan history (default false)
+                prompt: "Place a barcode inside the scan area", // Android
+                resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+                formats: "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+                orientation: "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+                disableAnimations: true, // iOS
+                disableSuccessBeep: false // iOS and Android
             }
         );
     })
