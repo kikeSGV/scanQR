b377a253e8b091f244b3658d19f9cd32e7499a87
kikeSGV <kike@sgv.es>
2020-01-24 08:44:04 +0100

    alert message

--- www/js/app.js
+++ www/js/app.js
@@ -6,16 +6,21 @@ function onDeviceReady() {
      $('#scan').click( function() 
 		{
 		  alert('hi');
-		  cordova.plugins.barcodeScanner.scan(
-		  function (result) {
-			  alert("We got a barcode\n" +
-					"Result: " + result.text + "\n" +
-					"Format: " + result.format + "\n" +
-					"Cancelled: " + result.cancelled);			  
-		  }, 
-		  function (error) {
-			  alert("Scanning failed: " + error);
-		  });
+		  try{
+			  cordova.plugins.barcodeScanner.scan(
+			  function (result) {
+				  alert("We got a barcode\n" +
+						"Result: " + result.text + "\n" +
+						"Format: " + result.format + "\n" +
+						"Cancelled: " + result.cancelled);			  
+			  }, 
+			  function (error) {
+				  alert("Scanning failed: " + error);
+			  });
+		  }
+		  catch(err){
+			  alert(err.message);
+		  }
 		}
 	 );
 }
