1c5469d62628b580122863150718f3bcbda648fa
kikeSGV <kike@sgv.es>
2020-01-24 10:14:23 +0100

    prueba 3

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
-# Cordova_QR_Scanner
-
-A sample cordova app to scan QR code and Barcode
-
-Add com.phonegap.plugins.barcodescanner plugin to test the app
+# QR-SCANNER
+qr scanner for android devices 
--- platforms/android/CordovaLib/src/org/apache/cordova/engine/SystemCookieManager.java
+++ platforms/android/CordovaLib/src/org/apache/cordova/engine/SystemCookieManager.java
@@ -37,9 +37,6 @@ class SystemCookieManager implements ICordovaCookieManager {
         webView = webview;
         cookieManager = CookieManager.getInstance();
 
-        //REALLY? Nobody has seen this UNTIL NOW?
-        cookieManager.setAcceptFileSchemeCookies(true);
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             cookieManager.setAcceptThirdPartyCookies(webView, true);
         }
@@ -57,13 +54,8 @@ class SystemCookieManager implements ICordovaCookieManager {
         return cookieManager.getCookie(url);
     }
 
-    @SuppressWarnings("deprecation")
     public void clearCookies() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            cookieManager.removeAllCookies(null);
-        } else {
-            cookieManager.removeAllCookie();
-        }
+        cookieManager.removeAllCookie();
     }
 
     public void flush() {
